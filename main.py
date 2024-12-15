c7e72e20511d634b7ec6f27548e2124d965f196b
karaman56 <166434324+karaman56@users.noreply.github.com>
2024-12-16 02:40:42 +0500

    Улучшил код после ревью
    - назвал токен специфическим названием
    - починил код
    - избавился вот комментариев
    - упростил создание словаря

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import requests
 import os
 from dotenv import load_dotenv
-from collections import defaultdict
 from terminaltables import AsciiTable
 
-
-def get_api_key():
+def get_sj_api_key():
     load_dotenv()
-    api_key = os.getenv('API_KEY')
-    if not api_key:
-        raise ValueError("API_KEY не установлен в переменных окружения.")
-    return api_key
+    return os.getenv('SJ_API_KEY')
 
 def get_languages():
     return [
@@ -33,24 +28,13 @@ def predict_salary(salary_from=0, salary_to=0):
         return salary_from * 1.2
     elif salary_to:
         return salary_to * 0.8
-    else:
-        return None
-
-
+    return None
 
 def predict_rub_salary(vacancy):
     salary = vacancy.get('salary')
-    if not salary:
+    if not salary or salary.get('currency') != 'RUR':
         return None
-
-    currency = salary.get('currency')
-    if currency != 'RUR':
-        return None
-
-    salary_from = salary.get('from')
-    salary_to = salary.get('to')
-
-    return predict_salary(salary_from, salary_to)
+    return predict_salary(salary.get('from'), salary.get('to'))
 
 def get_hh_statistics(languages):
     salary_statistics = {}
@@ -73,7 +57,7 @@ def get_hh_statistics(languages):
 
             hh_vacancies = response.json()
             if page == 0:
-                vacancies_found = (hh_vacancies.get('found', 0),2000)
+                vacancies_found = hh_vacancies.get('found', 0)
             if 'items' not in hh_vacancies or not hh_vacancies['items']:
                 break
 
@@ -84,7 +68,6 @@ def get_hh_statistics(languages):
                     vacancies_processed += 1
             page += 1
 
-
         average_salary = int(total_salary / vacancies_processed) if vacancies_processed else 0
         salary_statistics[language] = {
             "vacancies_found": vacancies_found,
@@ -93,10 +76,10 @@ def get_hh_statistics(languages):
         }
     return salary_statistics
 
-def search_programmer_vacancies(keyword, town_id, catalog_id, api_key):
+def search_programmer_vacancies(keyword, town_id, catalog_id, sj_api_key):
     url = 'https://api.superjob.ru/2.0/vacancies/'
     headers = {
-        'X-Api-App-Id': api_key,
+        'X-Api-App-Id': sj_api_key,
         'Content-Type': 'application/json'
     }
     vacancies = []
@@ -116,30 +99,27 @@ def search_programmer_vacancies(keyword, town_id, catalog_id, api_key):
             break
 
         sj_vacancies = response.json()
-        total_vacancies_found = sj_vacancies.get('total', 0)
         if 'objects' not in sj_vacancies or not sj_vacancies['objects']:
             break
         vacancies.extend(sj_vacancies.get('objects', []))
-        page += 1  # Не забудьте увеличивать номер страницы
+        page += 1
 
-    return vacancies, total_vacancies_found
+    return vacancies, sj_vacancies.get('total', 0)
 
 def predict_rub_salary_sj(vacancy):
-    salary_from = vacancy.get('payment_from')
-    salary_to = vacancy.get('payment_to')
-    return predict_salary(salary_from, salary_to)
+    return predict_salary(vacancy.get('payment_from'), vacancy.get('payment_to'))
 
-def get_sj_statistics(languages, api_key):
-    statistics = {}
-
-    for language in languages:
-        statistics[language] = {
+def get_sj_statistics(languages, sj_api_key):
+    statistics = {
+        language: {
             "vacancies_found": 0,
             "vacancies_processed": 0,
             "average_salary": 0
-        }
+        } for language in languages
+    }
 
-        vacancies, total_vacancies_found = search_programmer_vacancies(language, town_id=4, catalog_id=48, api_key=api_key)
+    for language in languages:
+        vacancies, _ = search_programmer_vacancies(language, town_id=4, catalog_id=48, sj_api_key=sj_api_key)
 
         for vacancy in vacancies:
             salary = predict_rub_salary_sj(vacancy)
@@ -168,10 +148,10 @@ def print_statistics_table(statistics, title):
     print(table.table)
 
 def main():
-    api_key = get_api_key()
+    sj_api_key = get_sj_api_key()
     languages = get_languages()
     hh_statistics = get_hh_statistics(languages)
-    sj_statistics = get_sj_statistics(languages, api_key)
+    sj_statistics = get_sj_statistics(languages, sj_api_key)
     print("Статистика по HeadHunter:")
     print_statistics_table(hh_statistics, "Язык программирования")
     print("\nСтатистика по SuperJob:")
